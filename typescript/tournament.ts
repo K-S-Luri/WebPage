647ebe7801af36818b9d62bde1675270d20ad892
K-S-Luri <tsururinn112358@gmail.com>
2019-08-29 19:56:11 +0900

    typescriptの導入

--- typescript/tournament.ts
+++ typescript/tournament.ts
@@ -1,16 +1,19 @@
-"use strict";
-var Player = /** @class */ (function () {
-    function Player(name, point, miss) {
+class Player {
+    name: string | null;
+    point: number | null;
+    miss: number | null;
+
+    constructor (name: string | null, point: number | null, miss: number | null) {
         this.name = name;
         this.point = point;
         this.miss = miss;
     }
-    return Player;
-}());
-function buildTournament(players) {
-    var base = document.getElementById("tournament");
-    var tableHTML = '<table class="tour-match">';
-    for (var i = 0; i < players.length; i++) {
+}
+
+function buildTournament(players: Player[]): void {
+    let base = document.getElementById("tournament");
+    let tableHTML = '<table class="tour-match">';
+    for (let i = 0; i < players.length; i++) {
         tableHTML += makeOneTr(players[i]);
     }
     tableHTML += "</table>";
@@ -18,22 +21,28 @@ function buildTournament(players) {
         base.insertAdjacentHTML('beforeend', tableHTML);
     }
 }
-function makeOneTr(player) {
-    var nameString = "";
+
+function makeOneTr(player: Player): string {
+    let nameString = "";
     if (player.name != null) {
         nameString = player.name;
     }
-    var pointString = "";
+    let pointString = "";
     if (player.point != null) {
         pointString = player.point.toString();
     }
-    var missString = "";
+    let missString = "";
     if (player.miss != null) {
         missString = player.miss.toString();
     }
-    return "<tr>\n    <td class=\"tour-name\">" + nameString + "</td>\n    <td class=\"tour-point\">" + pointString + "</td>\n    <td class=\"tour-miss\">" + missString + "</td>\n    </tr>";
+    return `<tr>
+    <td class="tour-name">${nameString}</td>
+    <td class="tour-point">${pointString}</td>
+    <td class="tour-miss">${missString}</td>
+    </tr>`;
 }
-document.addEventListener("DOMContentLoaded", function () {
+
+document.addEventListener("DOMContentLoaded", () => {
     buildTournament([new Player("wktk", 7, 0), new Player("ktkr", 7, 1), new Player("今北sangyou", 5, 2), new Player(null, null, null)]);
     buildTournament([new Player("wktk", 7, 0), new Player("ktkr", 7, 1), new Player("今北sangyou", 5, 2), new Player("kwsk", 4, 3)]);
 });
