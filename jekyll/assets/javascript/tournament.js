4a71455dfc2321a9aa9602ffc6c4cc0cdd5c65cc
K-S-Luri <tsururinn112358@gmail.com>
2019-09-14 19:58:47 +0900

    tournamentのクラス設計

--- jekyll/assets/javascript/tournament.js
+++ jekyll/assets/javascript/tournament.js
@@ -9,59 +9,108 @@ var TrPlace = /** @class */ (function () {
     };
     return TrPlace;
 }());
+var TrMatch = /** @class */ (function () {
+    function TrMatch(places, side, round, id) {
+        this.isDummy = false;
+        this.places = places;
+        this.side = side;
+        this.round = round;
+        this.id = id;
+    }
+    TrMatch.calcSize = function () {
+        var dummy = new TrMatch([null, null, null, null], "W", 0, 0);
+        dummy.isDummy = true;
+        dummy.draw();
+        var matches = document.getElementsByClassName("tour-match");
+        if (matches.length == 0) {
+            console.log("試合がないよ");
+            return;
+        }
+        var basematch = matches[0];
+        this.width = basematch.getBoundingClientRect().width;
+        this.height = basematch.getBoundingClientRect().height;
+        //dummyを作っているのがtournamentなのでparentは絶対あります
+        basematch.parentNode.removeChild(basematch);
+    };
+    Object.defineProperty(TrMatch.prototype, "top", {
+        get: function () {
+            if (this.isDummy) {
+                return 0;
+            }
+            function calcTop(round) {
+                if (round == 0) {
+                    return 0;
+                }
+                return 0;
+            }
+            return (TrMatch.height + 10) * this.id;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TrMatch.prototype, "left", {
+        get: function () {
+            if (this.isDummy) {
+                return 0;
+            }
+            return (TrMatch.width + 20) * this.round;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    TrMatch.prototype.draw = function () {
+        var base = document.getElementById("tournament");
+        var tableHTML = '<table class="tour-match" style="position: absolute;';
+        tableHTML += 'left:' + this.left.toString() + 'px;';
+        tableHTML += 'top:' + this.top + 'px;';
+        tableHTML += '">';
+        for (var i = 0; i < this.places.length; i++) {
+            tableHTML += this.makeOneTr(this.places[i]);
+        }
+        tableHTML += "</table>";
+        if (base != null) {
+            base.insertAdjacentHTML('beforeend', tableHTML);
+        }
+    };
+    TrMatch.prototype.makeOneTr = function (place) {
+        var nameString = "";
+        var pointString = "";
+        var missString = "";
+        var rankString = "";
+        if (place != null) {
+            nameString = place.name;
+            if (place.result != null) {
+                pointString = place.result.point.toString();
+                missString = place.result.miss.toString();
+                rankString = place.result.rank.toString();
+            }
+        }
+        return "<tr>\n        <td class=\"tour-name\">" + nameString + "</td>\n        <td class=\"tour-point\">" + pointString + "-" + missString + "</td>\n        <td class=\"tour-rank\">" + rankString + "</td>\n        </tr>";
+    };
+    return TrMatch;
+}());
 function buildTournament() {
     var base = document.getElementById("tournament");
     if (base != null) {
         base.style.width = "1000px";
         base.style.height = "1000px";
     }
-    var matches = document.getElementsByClassName("tour-match");
-    if (matches.length > 0) {
-        var match = matches[0];
-        var matchWidth = match.getBoundingClientRect().width;
-        var matchHeight = match.getBoundingClientRect().height;
-        console.log(matchWidth);
-        console.log(matchHeight);
-    }
-    else {
-        console.log("だめだよ");
-    }
-}
-function makeOneMatch(places) {
-    var base = document.getElementById("tournament");
-    var tableHTML = '<table class="tour-match">';
-    for (var i = 0; i < places.length; i++) {
-        tableHTML += makeOneTr(places[i]);
-    }
-    tableHTML += "</table>";
-    if (base != null) {
-        base.insertAdjacentHTML('beforeend', tableHTML);
-    }
-}
-function makeOneTr(place) {
-    var nameString = "";
-    var pointString = "";
-    var missString = "";
-    var rankString = "";
-    if (place != null) {
-        nameString = place.name;
-        if (place.result != null) {
-            pointString = place.result.point.toString();
-            missString = place.result.miss.toString();
-            rankString = place.result.rank.toString();
-        }
-    }
-    return "<tr>\n    <td class=\"tour-name\">" + nameString + "</td>\n    <td class=\"tour-point\">" + pointString + "-" + missString + "</td>\n    <td class=\"tour-rank\">" + rankString + "</td>\n    </tr>";
 }
 document.addEventListener("DOMContentLoaded", function () {
+    TrMatch.calcSize();
     var place1 = new TrPlace("wktk");
     var place2 = new TrPlace("ktkr");
     var place3 = new TrPlace("佐々木 忠次郎");
     var place4 = null;
-    makeOneMatch([place1, place2, place3, place4]);
+    var match1 = new TrMatch([place1, place2, place3, place4], "W", 0, 0);
+    match1.draw();
     place1.setResult(7, 0, 1);
     place2.setResult(3, 2, 4);
-    makeOneMatch([place1, place2, place3, place4]);
-    // buildTournament([new TrPlace("wktk", 7, 0), new TrPlace("ktkr", 7, 1), new TrPlace("今北sangyou", 5, 2), new TrPlace("kwsk", 4, 3)]);
+    var match2 = new TrMatch([place1, place2, place3, place4], "W", 0, 1);
+    match2.draw();
+    for (var i = 0; i < 8; i++) {
+        var match = new TrMatch([place1, place2, place3, place4], "W", i, i + 2);
+        match.draw();
+    }
     buildTournament();
 });
--- typescript/tournament.ts
+++ typescript/tournament.ts
@@ -13,8 +13,98 @@ class TrPlace {
         this.result = null;
     }
 
-    public setResult(point: number, miss: number, rank: number): void {
+    setResult(point: number, miss: number, rank: number): void {
         this.result = {point: point, miss: miss, rank: rank };
+    }
+}
+
+type TrSide = "W" | "L";
+
+class TrMatch {
+    places: (TrPlace | null)[];
+    side: TrSide;
+    round: number;
+    id: number;
+    isDummy: boolean = false;
+    static width: number;
+    static height: number;
+
+    constructor (places: (TrPlace | null)[], side: TrSide, round: number, id: number) {
+        this.places = places;
+        this.side = side;
+        this.round = round;
+        this.id = id;
+    }
+
+    static calcSize(): void {
+        let dummy = new TrMatch([null, null, null, null], "W", 0, 0);
+        dummy.isDummy = true;
+        dummy.draw();
+        let matches = document.getElementsByClassName("tour-match");
+        if (matches.length == 0) {
+            console.log("試合がないよ");
+            return;
+        }
+        let basematch = matches[0] as HTMLElement;
+        this.width = basematch.getBoundingClientRect().width;
+        this.height = basematch.getBoundingClientRect().height;
+        //dummyを作っているのがtournamentなのでparentは絶対あります
+        basematch.parentNode!.removeChild(basematch);
+    }
+
+    get top(): number {
+        if (this.isDummy) {
+            return 0;
+        }
+        function calcTop(round: number): number {
+            if (round == 0) {
+                return 0;
+            }
+            return 0;
+        }
+        return (TrMatch.height + 10) * this.id;
+    }
+
+    get left(): number {
+        if (this.isDummy) {
+            return 0;
+        }
+        return (TrMatch.width + 20) * this.round;
+    }
+
+    draw() {
+        let base = document.getElementById("tournament");
+        let tableHTML = '<table class="tour-match" style="position: absolute;';
+        tableHTML += 'left:' + this.left.toString() + 'px;';
+        tableHTML += 'top:' + this.top + 'px;';
+        tableHTML += '">';
+        for (let i = 0; i < this.places.length; i++) {
+            tableHTML += this.makeOneTr(this.places[i]);
+        }
+        tableHTML += "</table>";
+        if (base != null) {
+            base.insertAdjacentHTML('beforeend', tableHTML);
+        }
+    }
+
+    private makeOneTr(place: TrPlace |  null): string {
+        let nameString = "";
+        let pointString = "";
+        let missString = "";
+        let rankString = "";
+        if (place != null) {
+            nameString = place.name;
+            if (place.result != null) {
+                pointString = place.result.point.toString();
+                missString = place.result.miss.toString();
+                rankString = place.result.rank.toString();
+            }
+        }
+        return `<tr>
+        <td class="tour-name">${nameString}</td>
+        <td class="tour-point">${pointString}-${missString}</td>
+        <td class="tour-rank">${rankString}</td>
+        </tr>`;
     }
 }
 
@@ -24,59 +114,23 @@ function buildTournament(): void {
         base.style.width = "1000px";
         base.style.height = "1000px";
     }
-    let matches = document.getElementsByClassName("tour-match");
-    if (matches.length > 0) {
-        let match = matches[0] as HTMLElement;
-        let matchWidth = match.getBoundingClientRect().width;
-        let matchHeight = match.getBoundingClientRect().height;
-        console.log(matchWidth);
-        console.log(matchHeight);
-    } else {
-        console.log("だめだよ")
-    }
-}
-
-function makeOneMatch(places: (TrPlace | null)[]): void {
-    let base = document.getElementById("tournament");
-    let tableHTML = '<table class="tour-match">';
-    for (let i = 0; i < places.length; i++) {
-        tableHTML += makeOneTr(places[i]);
-    }
-    tableHTML += "</table>";
-    if (base != null) {
-        base.insertAdjacentHTML('beforeend', tableHTML);
-    }
-}
-
-function makeOneTr(place: TrPlace |  null): string {
-    let nameString = "";
-    let pointString = "";
-    let missString = "";
-    let rankString = "";
-    if (place != null) {
-        nameString = place.name;
-        if (place.result != null) {
-            pointString = place.result.point.toString();
-            missString = place.result.miss.toString();
-            rankString = place.result.rank.toString();
-        }
-    }
-    return `<tr>
-    <td class="tour-name">${nameString}</td>
-    <td class="tour-point">${pointString}-${missString}</td>
-    <td class="tour-rank">${rankString}</td>
-    </tr>`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    TrMatch.calcSize();
     const place1:TrPlace = new TrPlace("wktk");
     const place2:TrPlace = new TrPlace("ktkr");
     const place3:TrPlace = new TrPlace("佐々木 忠次郎");
     const place4 = null;
-    makeOneMatch([place1, place2, place3, place4]);
+    let match1 = new TrMatch([place1, place2, place3, place4], "W", 0, 0);
+    match1.draw();
     place1.setResult(7, 0, 1);
     place2.setResult(3, 2, 4);
-    makeOneMatch([place1, place2, place3, place4]);
-    // buildTournament([new TrPlace("wktk", 7, 0), new TrPlace("ktkr", 7, 1), new TrPlace("今北sangyou", 5, 2), new TrPlace("kwsk", 4, 3)]);
+    let match2 = new TrMatch([place1, place2, place3, place4], "W", 0, 1);
+    match2.draw();
+    for (let i = 0; i < 8; i++) {
+        let match = new TrMatch([place1, place2, place3, place4], "W", i, i + 2);
+        match.draw();
+    }
     buildTournament();
 });
