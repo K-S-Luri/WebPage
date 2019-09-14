863efd0fdd2fb356c12cbeb685321db3b9b87a74
K-S-Luri <tsururinn112358@gmail.com>
2019-09-14 18:04:42 +0900

    docsの作成

---
+++ docs/assets/javascript/tournament.js
@@ -0,0 +1,48 @@
+"use strict";
+var TrPlace = /** @class */ (function () {
+    function TrPlace(name) {
+        this.name = name;
+        this.result = null;
+    }
+    TrPlace.prototype.setResult = function (point, miss, rank) {
+        this.result = { point: point, miss: miss, rank: rank };
+    };
+    return TrPlace;
+}());
+function buildTournament(places) {
+    var base = document.getElementById("tournament");
+    var tableHTML = '<table class="tour-match">';
+    for (var i = 0; i < places.length; i++) {
+        tableHTML += makeOneTr(places[i]);
+    }
+    tableHTML += "</table>";
+    if (base != null) {
+        base.insertAdjacentHTML('beforeend', tableHTML);
+    }
+}
+function makeOneTr(place) {
+    var nameString = "";
+    var pointString = "";
+    var missString = "";
+    var rankString = "";
+    if (place != null) {
+        nameString = place.name;
+        if (place.result != null) {
+            pointString = place.result.point.toString();
+            missString = place.result.miss.toString();
+            rankString = place.result.rank.toString();
+        }
+    }
+    return "<tr>\n    <td class=\"tour-name\">" + nameString + "</td>\n    <td class=\"tour-point\">" + pointString + "-" + missString + "</td>\n    <td class=\"tour-rank\">" + rankString + "</td>\n    </tr>";
+}
+document.addEventListener("DOMContentLoaded", function () {
+    var place1 = new TrPlace("wktk");
+    var place2 = new TrPlace("ktkr");
+    var place3 = new TrPlace("佐々木 忠次郎");
+    var place4 = null;
+    buildTournament([place1, place2, place3, place4]);
+    place1.setResult(7, 0, 1);
+    place2.setResult(3, 2, 4);
+    buildTournament([place1, place2, place3, place4]);
+    // buildTournament([new TrPlace("wktk", 7, 0), new TrPlace("ktkr", 7, 1), new TrPlace("今北sangyou", 5, 2), new TrPlace("kwsk", 4, 3)]);
+});
